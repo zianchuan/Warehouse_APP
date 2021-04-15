fb3fe516b8aff12c5e52f6e7d1f06922103af758
SMeiFoong <meifoong1113@gmail>
2021-04-15 22:50:22 +0800

    new branch for shipment

---
+++ app/src/main/java/com/example/warehouse_app/MF_Shipment.kt
@@ -0,0 +1,11 @@
+package com.example.warehouse_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MF_Shipment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_m_f__shipment)
+    }
+}
